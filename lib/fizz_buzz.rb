5a48f2b5a690946d376cad2b3326bf991687bc9b
tbsmcd <tbsmcd@gmail.com>
2019-08-14 23:25:40 +0900

    第3章

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -4,16 +4,17 @@ def fizz_buzz(n)
   elsif n % 3 == 0
     'Fizz'
   elsif n % 5 == 0
-    'buzz'
+    'Buzz'
   else
     n.to_s
   end
 end
 
-puts fizz_buzz(1)
-puts fizz_buzz(2)
-puts fizz_buzz(3)
-puts fizz_buzz(4)
-puts fizz_buzz(5)
-puts fizz_buzz(6)
-puts fizz_buzz(15)
+
+# puts fizz_buzz(1)
+# puts fizz_buzz(2)
+# puts fizz_buzz(3)
+# puts fizz_buzz(4)
+# puts fizz_buzz(5)
+# puts fizz_buzz(6)
+# puts fizz_buzz(15)
